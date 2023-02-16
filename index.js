9952d9c82dec48f81740d5ee86d420a04e2ccb36
nitika807 <nitika807sharma@gmail.com>
2023-02-16 04:28:30 -0500

    altered index.html and main.css

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
     <title>Title</title>
 </head>
 <body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <input type="text" placeholder="Search..">
+</div>
 //This document consists of the javascript codes for the webpage
 </body>
 </html>
